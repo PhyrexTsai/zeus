b2d47f019fa806b1385d2d9d2befa1c30f6ea80e
Yung chieh Tsai <x01ep23i@hotmail.com>
2016-11-03 22:52:28 +0800

    update

--- scripts/flickr.js
+++ scripts/flickr.js
@@ -20,15 +20,15 @@ module.exports = function(robot) {
         var target_url = "https://www.flickr.com/search/?text=" + topic;
         robot.http(target_url).get()(function(err, res, body) {
             if (res.statusCode!= 200) {
-				        return response.send("Flickr loading error!");
+	         return response.send("Flickr loading error!");
             }
             var $ = cheerio.load(body);
-			      var tag = $("div.photo-list-photo-view")
+	    var tag = $("div.photo-list-photo-view")
             for (var i = 0; i < 10; i++) {
-              	var image_url = $(tag[i]).css("background-image");
-                var image = image_url.substring(4, image_url.length - 1);
-				        response.send("http:" + image);
-			      }
+               	 var image_url = $(tag[i]).css("background-image");
+                 var image = image_url.substring(4, image_url.length - 1);
+	         response.send("http:" + image);
+	    }
         })
     });
 }
